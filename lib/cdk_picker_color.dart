4f38f5027e7c2e2f3792db687ce59829adc09a02
gbiagi <geanfrancobiagi@hotmail.com>
2025-01-29 17:38:26 +0100

    .

--- lib/cdk_picker_color.dart
+++ lib/cdk_picker_color.dart
@@ -41,120 +41,240 @@ class CDKPickerColorState extends State<CDKPickerColor> {
     _rgbBlue = widget.color.blue.toDouble();
     _rgbAlpha = widget.color.alpha.toDouble() / 255;
 
-    return SizedBox(
-        width: 225,
-        child: Column(
+    return Container(
+        width: 500,
+        height: 500,
+        child: Row(
+/*                       Container(
+              width: 20,
+              height: 20,
+              decoration: BoxDecoration(
+                color: widget.color,
+                border: Border.all(
+                  color: CDKTheme.grey100,
+                  width: 1,
+                ),
+                borderRadius: BorderRadius.circular(4.0),
+              ),
+            ),
+            const SizedBox(height: 8), */
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            CDKPickerColorHSV(
-                value: widget.color,
-                onChanged: (value) {
-                  widget.onChanged!.call(value);
-                }),
-            const SizedBox(height: 8),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              SizedBox(
-                  width: 65,
-                  child: CDKFieldNumeric(
-                    value: _rgbRed,
-                    min: 0,
-                    max: 255,
-                    decimals: 0,
-                    increment: 1,
-                    units: "R",
-                    onValueChanged: (value) {
-                      _rgbRed = value;
-                      _callbackRGB();
-                    },
-                  )),
-              SizedBox(
-                  width: 65,
-                  child: CDKFieldNumeric(
-                    value: _rgbGreen,
-                    min: 0,
-                    max: 255,
-                    decimals: 0,
-                    increment: 1,
-                    units: "G",
-                    onValueChanged: (value) {
-                      _rgbGreen = value;
-                      _callbackRGB();
-                    },
-                  )),
-              SizedBox(
-                  width: 65,
-                  child: CDKFieldNumeric(
-                    value: _rgbBlue,
-                    min: 0,
-                    max: 255,
-                    decimals: 0,
-                    increment: 1,
-                    units: "B",
-                    onValueChanged: (value) {
-                      _rgbBlue = value;
-                      _callbackRGB();
-                    },
-                  )),
-            ]),
-            const SizedBox(height: 8),
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              SizedBox(
-                  width: 65,
-                  child: CDKFieldNumeric(
-                    value: _rgbAlpha,
-                    min: 0,
-                    max: 1,
-                    decimals: 2,
-                    increment: 0.05,
-                    units: "A",
-                    onValueChanged: (value) {
-                      _rgbAlpha = value;
-                      _callbackRGB();
-                    },
-                  )),
-              SizedBox(
-                  width: 65,
-                  child: CDKFieldColorHex(
-                    value: widget.color.value,
-                    onValueChanged: (value) {
-                      Color color = Color(value | 0xFF000000);
-                      _rgbRed = color.red.toDouble();
-                      _rgbGreen = color.green.toDouble();
-                      _rgbBlue = color.blue.toDouble();
-                      _callbackRGB();
-                    },
-                  )),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(4.0),
-                child: Container(
-                  width: 65,
-                  height: 22,
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: CDKTheme.grey100, // Color del border
-                      width: 1, // Amplada del border
+            Column(children: [
+              const Text("Basic Colors:"),
+              Row(
+                children: [
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
-                    borderRadius: BorderRadius.circular(4.0),
                   ),
-                  child: CustomPaint(
-                    painter: CDKUtilShaderGrid(7),
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Container(
-                          color: widget.color,
-                        )),
-                        Container(
-                          width: 20,
-                          color: Color(widget.color.value | 0xFF000000),
-                        ),
-                      ],
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
                     ),
+                  ),
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                  const SizedBox(width: 4),
+                  Container(
+                    width: 20,
+                    height: 20,
+                    decoration: BoxDecoration(
+                      color: widget.color,
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+            ]),
+            Column(children: [
+              Expanded(
+                child: RotatedBox(
+                  quarterTurns: 3,
+                  child: CDKPickerColorHSV(
+                    value: widget.color,
+                    onChanged: (value) {
+                      widget.onChanged!.call(value);
+                    },
                   ),
                 ),
               ),
-            ]),
+              const SizedBox(height: 8),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                SizedBox(
+                    width: 65,
+                    child: CDKFieldNumeric(
+                      value: _rgbRed,
+                      min: 0,
+                      max: 255,
+                      decimals: 0,
+                      increment: 1,
+                      units: "R",
+                      onValueChanged: (value) {
+                        _rgbRed = value;
+                        _callbackRGB();
+                      },
+                    )),
+                SizedBox(
+                    width: 65,
+                    child: CDKFieldNumeric(
+                      value: _rgbGreen,
+                      min: 0,
+                      max: 255,
+                      decimals: 0,
+                      increment: 1,
+                      units: "G",
+                      onValueChanged: (value) {
+                        _rgbGreen = value;
+                        _callbackRGB();
+                      },
+                    )),
+                SizedBox(
+                    width: 65,
+                    child: CDKFieldNumeric(
+                      value: _rgbBlue,
+                      min: 0,
+                      max: 255,
+                      decimals: 0,
+                      increment: 1,
+                      units: "B",
+                      onValueChanged: (value) {
+                        _rgbBlue = value;
+                        _callbackRGB();
+                      },
+                    )),
+              ]),
+              const SizedBox(height: 8),
+              Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                SizedBox(
+                    width: 65,
+                    child: CDKFieldNumeric(
+                      value: _rgbAlpha,
+                      min: 0,
+                      max: 1,
+                      decimals: 2,
+                      increment: 0.05,
+                      units: "A",
+                      onValueChanged: (value) {
+                        _rgbAlpha = value;
+                        _callbackRGB();
+                      },
+                    )),
+                SizedBox(
+                    width: 65,
+                    child: CDKFieldColorHex(
+                      value: widget.color.value,
+                      onValueChanged: (value) {
+                        Color color = Color(value | 0xFF000000);
+                        _rgbRed = color.red.toDouble();
+                        _rgbGreen = color.green.toDouble();
+                        _rgbBlue = color.blue.toDouble();
+                        _callbackRGB();
+                      },
+                    )),
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(4.0),
+                  child: Container(
+                    width: 65,
+                    height: 22,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: CDKTheme.grey100,
+                        width: 1,
+                      ),
+                      borderRadius: BorderRadius.circular(4.0),
+                    ),
+                    child: CustomPaint(
+                      painter: CDKUtilShaderGrid(7),
+                      child: Row(
+                        children: [
+                          Expanded(
+                              child: Container(
+                            color: widget.color,
+                          )),
+                          Container(
+                            width: 20,
+                            color: Color(widget.color.value | 0xFF000000),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
+            ])
           ],
         ));
   }
